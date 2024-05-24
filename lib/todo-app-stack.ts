b493228089647fff34233e864a0948233e56772c
redfit <nagita.0724.0812@gmail.com>
2024-05-24 23:59:59 +0900

    Create and Apply a Layout to Astro Pages

--- lib/todo-app-stack.ts
+++ lib/todo-app-stack.ts
@@ -1,19 +1,14 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import * as lambdaNodejs from 'aws-cdk-lib/aws-lambda-nodejs'
 
 export class TodoAppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'TodoAppQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const helloWorldFunction = new lambdaNodejs.NodejsFunction(this, 'HelloWorld', {
+      entry: "lambda/hello-world.ts",
+      handler: "handler"
     });
-
-    const topic = new sns.Topic(this, 'TodoAppTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
